fd1ce5c8fec61899f5f0c22da7fc95e7118bcd90
Angie <angie.monnye@gmail.com>
2023-07-10 12:04:41 +0000

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,27 @@
-#!/usr/bin/python3
-# script that distributes an archive to the web servers, using the function do_deploy:
+cript that distributes an archive to the web servers
+# using the function do_deploy:
 """A module for Fabric script that generates a .tgz archive."""
 import os
 from datetime import datetime
 from fabric.api import local
 
+
 def do_pack():
-    """ Archiving static files """
+    """Archiving static files"""
     date = datetime.now()
     file = "versions/web_static_{}{}{}{}{}{}.tgz".format(date.year,
-        date.month,
-        date.day,
-        date.hour,
-        date.minute,
-        date.second)
+                                                         date.month,
+                                                         date.day,
+                                                         date.hour,
+                                                         date.minute,
+                                                         date.second)
 
     if os.path.isdir("versions") is False:
-       if local("mkdir -p versions").failed is True:
+        if local("mkdir -p versions").failed is True:
             return None
 
     if local("tar -czvf {} web_static".format(file)).failed is True:
-            return None
+        return None
 
     return file
+
