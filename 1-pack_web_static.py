1832183c4c2b9aa9a293d95ae0dc703afea2c7f6
Angie <angie.monnye@gmail.com>
2023-07-14 00:45:48 +0000

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,15 +10,21 @@ from fabric.api import local
 def do_pack():
     """Archiving static files"""
     date = datetime.now()
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
     file = "versions/web_static_{}{}{}{}{}{}.tgz".format(date.year,
                                                          date.month,
                                                          date.day,
                                                          date.hour,
                                                          date.minute,
                                                          date.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
-        return None
+    
+    try:
+        print("Packing web_static to {}".format(file))
+        local("tar -cvzf {} web_static".format(file))
+        size = os.stat(file).st_size
+        print("web_static packed: {} -> {} Bytes".format(file, size))
+    except Exception:
+        file = None
     return file
